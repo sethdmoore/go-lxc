f9b499545d73dbf6ec838760e5f0f677217324f9
Seth Moore <sethdmoore@gmail.com>
2016-04-18 23:37:56 -0400

    Fix regression from merging differing branches

--- container.go
+++ container.go
@@ -454,10 +454,6 @@ func (c *Container) Start() error {
 
 // Execute executes the given command in a temporary container.
 func (c *Container) Execute(args ...string) ([]byte, error) {
-	if err := c.makeSure(isNotDefined); err != nil {
-	        return nil, err
-	}
-
 	var output []byte
 	var err error
 	cargs := []string{"lxc-execute", "-n", c.Name(), "-P", c.ConfigPath(), "--"}
